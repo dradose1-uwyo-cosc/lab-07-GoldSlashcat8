a4d0004e8f8573ad07523898d009a348e0075c51
Chauncy Hendon <chendon2@uwyo.edu>
2024-10-31 15:27:47 +0000

    Complete

--- cosc1010-lab07.py
+++ cosc1010-lab07.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Chauncy Hendon
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
+# 10/31/24
+# Lab 7
+# Lab Section: 12 
 # Sources, people worked with, help given to: 
 # your
 # comments
@@ -18,8 +18,23 @@
 # You will continue to prompt the user until a proper integer value is entered
 
 factorial = 1
+factorial_calc= 1
+fac_true = True
 
-print(f"The result of the factorial based on the given bound is {factorial}")
+while fac_true:
+    factorial_input = input("Give a positive number to then be facorialed: ")
+    if factorial_input.isdigit() == True:
+        factorial = int(factorial_input)
+        factorial_calc = int(factorial_input)
+        while factorial > 1:
+            factorial_calc = factorial_calc * (factorial-1)
+            factorial -= 1
+        fac_true = False
+    else:
+        print("Please input a positive number instead")
+
+
+print(f"The result of the factorial based on the given bound is {factorial_calc}")
 
 print("*"*75)
 # Create a while loop that prompts a user for input of an integer values
@@ -38,8 +53,54 @@ print("*"*75)
 # The sum should start at 0 
 
 num_sum = 0 
+fixed_input = ""
+user_i_list = []
+is_neg = False
+is_number = True
+
+while True:
+    user_input = input("Please give a positive or negative value, type exit to leave: ")
+
+    fixed_input = ""
+    is_neg = False
+    user_i_list = []
+    is_number = True
+
+    if user_input == "exit":
+        break
+
+    for value in user_input:
+        user_i_list.append(value)
+
+    if user_i_list[0].isdigit():
+        is_number = True
+    elif user_i_list[0] == "-":
+        is_neg = True
+    else:
+        is_number = False
+        print("Input a number next time")
+
+    for i in user_i_list[1:]:
+        if i.isdigit() != True:
+            print("Please input a number next time")
+            is_number = False
+            
+    if is_number:
+        if user_i_list[0]== "-":
+            is_neg = True
+            for i in user_i_list[1:]:
+                fixed_input = fixed_input + i
+        else:
+            for i in user_i_list:
+                fixed_input = fixed_input + i
+
+        if is_neg:
+            num_sum += -int(fixed_input)
+        else:
+            num_sum += int(fixed_input)
 
 print(f"Your final sum is {num_sum}")
+
 
 print("*"*75)
 # Now you will be creating a two operand calculator
@@ -58,5 +119,112 @@ print("*"*75)
     # So, it should function the same for `5 + 6` as `5+6`
 # Print the result of the equation
 # Again, loop through prompting the user for input until `exit` in any casing is input 
+
+operators_list = ["+","-","/","*","%"]
+calculator_input = input("Give an operand operator operand statement: ")
+operator_used = ""
+final_problem = None
+final_problem_split = ""
+result = None
+number_of_nums = 0
+
+for item in calculator_input:
+    if item.isdigit():
+        number_of_nums += 1
+    for operator in operators_list:
+        if item == operator:
+            operator_used = operator
+
+if number_of_nums >= 2:
+    number_of_nums = 0
+
+    #+
+    if operator_used == "+":
+        final_problem = calculator_input.split("+")
+        for item in final_problem:
+            final_problem_split += item
+        final_problem_split = final_problem_split.split()
+        print(final_problem_split)
+
+        for item in final_problem_split:
+            if item.isdigit():
+                number_of_nums += 1
+        if number_of_nums == 2:
+            result = int(final_problem_split[0]) + int(final_problem_split[1])
+        else:
+            print("Please input an operand operator operand statement")
+
+    #-
+    if operator_used == "-":
+        final_problem = calculator_input.split("-")
+        for item in final_problem:
+            final_problem_split += item
+        final_problem_split = final_problem_split.split()
+        print(final_problem_split)
+
+        for item in final_problem_split:
+            if item.isdigit():
+                number_of_nums += 1
+        if number_of_nums == 2:
+            result = int(final_problem_split[0]) - int(final_problem_split[1])
+        else:
+            print("Please input an operand operator operand statement")
+
+    #*
+    if operator_used == "*":
+        final_problem = calculator_input.split("*")
+        for item in final_problem:
+            final_problem_split += item
+        final_problem_split = final_problem_split.split()
+        print(final_problem_split)
+
+        for item in final_problem_split:
+            if item.isdigit():
+                number_of_nums += 1
+        if number_of_nums == 2:
+            result = int(final_problem_split[0]) * int(final_problem_split[1])
+        else:
+            print("Please input an operand operator operand statement")
+
+    #/
+    if operator_used == "/":
+        final_problem = calculator_input.split("/")
+        for item in final_problem:
+            final_problem_split += item
+        final_problem_split = final_problem_split.split()
+        print(final_problem_split)
+
+        for item in final_problem_split:
+            if item.isdigit():
+                number_of_nums += 1
+        if number_of_nums == 2:
+            result = int(final_problem_split[0]) / int(final_problem_split[1])
+        else:
+            print("Please input an operand operator operand statement")
+
+    #%
+    if operator_used == "%":
+        final_problem = calculator_input.split("%")
+        for item in final_problem:
+            final_problem_split += item
+        final_problem_split = final_problem_split.split()
+        print(final_problem_split)
+
+        for item in final_problem_split:
+            if item.isdigit():
+                number_of_nums += 1
+        if number_of_nums == 2:
+            result = int(final_problem_split[0]) % int(final_problem_split[1])
+        else:
+            print("Please input an operand operator operand statement")
+
+print("The result of your calculation is:")
+print(result)
+
+    
+
+
+
+
 
         
